f33ce618370953ba27e8874e7967a64a61b4ce60
tmt <alkaloid.btx@gmail.com>
2013-11-22 20:04:25 +0100

    Updated main and CMakelists

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 
+#include "KeyboardController.hpp"
+
 int main()
 {
     sf::Window window(sf::VideoMode(800, 600), "My window");
@@ -11,7 +13,11 @@ int main()
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
-        {
+		{
+			if(event.type == sf::Event::KeyPressed) {
+				std::cout << "PRESS" << std::endl;
+			}
+
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
