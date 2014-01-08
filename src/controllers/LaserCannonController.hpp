99ea2466a6bcf8d9e8954d1ef0f3603334a0bdd1
tmt <alkaloid.btx@gmail.com>
2014-01-08 17:20:18 +0100

    lasercannoncontroller comments

--- src/controllers/LaserCannonController.hpp
+++ src/controllers/LaserCannonController.hpp
@@ -11,13 +11,37 @@ namespace Controller {
 
 class LaserCannonController {
 	public:
+		/**
+		@param laserCannon The lasercannon this controller has to control. The controller will own the pointer.
+		*/
 		LaserCannonController(Model::LaserCannon* laserCannon);
 
+		/**
+		* Move left.
+		*
+		* @param dt The delta time.
+		*/
 		void moveLeft(double dt);
+
+		/**
+		* Move right.
+		*
+		* @param dt The delta time.
+		*/
 		void moveRight(double dt);
 
+		/**
+		* Get the position.
+		*
+		* @return The position.
+		*/
 		Coordinate getPosition();
 
+		/**
+		* Checks if the lasercannon is alive.
+		*
+		* @return True if alive, else false.
+		*/
 		bool isAlive() const;
 
 	private:
