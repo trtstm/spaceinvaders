b847e10ad146fad03bcb0ae42c9c8e051911f9ef
tmt <alkaloid.btx@gmail.com>
2014-01-08 17:18:44 +0100

    bulletcontroller comments

--- src/controllers/BulletController.hpp
+++ src/controllers/BulletController.hpp
@@ -9,16 +9,44 @@ namespace Controller {
 
 class BulletController {
 	public:
+		/**
+		@param bullet The bullet this controller has to control. The controller will own the pointer.
+		*/
 		BulletController(Model::Bullet bullet);
 
+		/**
+		* Update the alien.
+		*
+		* @param dt The delta time.
+		*/
 		void update(double dt);
 
+		/**
+		* Get the position.
+		*
+		* @return The position.
+		*/
 		Coordinate getPosition() const;
 
+		/**
+		* Get the collision rectangle.
+		*
+		* @return The collision rectangle.
+		*/
 		sf::Rect<double> getCollisionRectangle() const;
 
+		/**
+		* Checks if the bullet is alive.
+		*
+		* @return True if alive, else false.
+		*/
 		bool isAlive() const;
 
+		/**
+		* Get the bullet this controller controls.
+		*
+		* @return The bullet.
+		*/
 		Model::Bullet& getBullet();
 
 	private:
