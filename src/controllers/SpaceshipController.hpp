5ef4b433e87744df8ceba4e467d5f49c3c856a7f
tmt <alkaloid.btx@gmail.com>
2014-01-08 17:21:58 +0100

    spaceshipcontroller comments

--- src/controllers/SpaceshipController.hpp
+++ src/controllers/SpaceshipController.hpp
@@ -13,17 +13,60 @@ class SpaceshipController {
 	public:
 		SpaceshipController(Model::Spaceship* spaceship);
 
+		/**
+		* Move left.
+		*
+		* @param dt The delta time.
+		*/
 		void moveLeft(double dt);
+
+		/**
+		* Move right.
+		*
+		* @param dt The delta time.
+		*/
 		void moveRight(double dt);
+
+		/**
+		* Move down.
+		*
+		* @param dt The delta time.
+		*/
 		void moveDown(double dt);
 
+		/**
+		* Get the position.
+		*
+		* @return The position.
+		*/
 		Coordinate getPosition();
+
+		/**
+		* Set the position.
+		*
+		* @param position The position.
+		*/
 		void setPosition(Coordinate position);
 
+		/**
+		* Update the alien.
+		*
+		* @param dt The delta time.
+		*/
 		void update(double dt);
 
+		/**
+		* Checks if the bullet is alive.
+		*
+		* @return True if alive, else false.
+		*/
 		bool isAlive() const;
 
+		/**
+		* Get the spaceship this controller controls.
+		*
+		* @return The spaceship.
+		*/
 		Model::Spaceship& getSpaceship();
 
 	private:
