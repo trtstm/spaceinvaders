f912d7a4818ee8f13359fba4f3e920758c481fdd
tmt <alkaloid.btx@gmail.com>
2014-01-08 17:25:40 +0100

    factories comments

--- src/factories/DefaultEntityFactory.hpp
+++ src/factories/DefaultEntityFactory.hpp
@@ -7,8 +7,29 @@ namespace Factory {
 
 class DefaultEntityFactory : public EntityFactory {
 	public:
+		/**
+		* Create a new spaceship.
+		*
+		* @param position The position.
+		* @return The spaceship.
+		*/
 		Model::Spaceship* newSpaceship(const Coordinate& position);
+
+		/**
+		* Create a new lasercannon.
+		*
+		* @param position The position.
+		* @return The lasercannon.
+		*/
 		Model::LaserCannon* newLaserCannon(const Coordinate& position);
+
+		/**
+		* Create a new alien.
+		*
+		* @param position The position.
+		* @param position The speed.
+		* @return The alien.
+		*/
 		Model::Alien* newAlien(const Coordinate& position, double speed);
 };
 
--- src/factories/EntityFactory.hpp
+++ src/factories/EntityFactory.hpp
@@ -11,8 +11,31 @@ namespace Factory {
 
 class EntityFactory {
 	public:
+		virtual ~EntityFactory() = 0;
+
+		/**
+		* Create a new spaceship.
+		*
+		* @param position The position.
+		* @return The spaceship.
+		*/
 		virtual Model::Spaceship* newSpaceship(const Coordinate& position) = 0;
+
+		/**
+		* Create a new lasercannon.
+		*
+		* @param position The position.
+		* @return The lasercannon.
+		*/
 		virtual Model::LaserCannon* newLaserCannon(const Coordinate& position) = 0;
+
+		/**
+		* Create a new alien.
+		*
+		* @param position The position.
+		* @param position The speed.
+		* @return The alien.
+		*/
 		virtual Model::Alien* newAlien(const Coordinate& position, double speed) = 0;
 };
 
